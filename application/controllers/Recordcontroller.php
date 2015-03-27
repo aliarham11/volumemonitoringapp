720780b1389dd98781852c23d6d78b1c60f3cc74
aliarham11 <aliarham.skom@gmail.com>
2015-03-27 13:50:55 +0700

    adding post method

--- application/controllers/Recordcontroller.php
+++ application/controllers/Recordcontroller.php
@@ -29,4 +29,27 @@ class Recordcontroller extends CI_Controller {
 		echo "Success gan";
 		# code...
 	}
+
+	public function postRecordData()
+	{
+		$this->load->model('Recordmodel');
+		$vehicle_id = 'SPBU1002';
+		$height = $this->input->post('height');
+		$latitude = $this->input->post('latitude');
+		$longitude = $this->input->post('longitude');
+		$altitude = "0.000";
+		$timestamp = date("Y-m-d h:i:s");
+
+		$data = array(
+			'vehicle_id' => $vehicle_id,
+			'liquid_level' => $height,
+			'latitude' => $latitude,
+			'longitude' => $longitude,
+			'altitude' => $altitude,
+			'timestamp' => $timestamp,
+		 );
+
+		$this->Recordmodel->insertRecordData($data);
+		echo "Success gan";
+	}
 }
