d6aee092af03370db618e442603dc42304ec3869
aliarham11 <aliarham.skom@gmail.com>
2015-04-09 09:11:12 +0700

    change url

--- application/views/head.php
+++ application/views/head.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
 	<title>Tracking Data</title>
-	<script src="<?php echo base_url();?>/assets/js/jquery.js"></script>
-	<script src="<?php echo base_url();?>/assets/js/jquery-1.10.1.min.js"></script>
-	<script src="<?php echo base_url();?>/assets/js/jquery-1.11.1.min.js"></script>
-	<link rel="stylesheet" href="<?php echo base_url();?>/assets/leaflet/leaflet.css" />
-	<script src="<?php echo base_url();?>/assets/leaflet/leaflet.js"></script>
-	<script src="<?php echo base_url();?>/assets/leaflet/leaflet-src.js"></script>
+	<script src="<?php echo base_url();?>assets/js/jquery.js"></script>
+	<script src="<?php echo base_url();?>assets/js/jquery-1.10.1.min.js"></script>
+	<script src="<?php echo base_url();?>assets/js/jquery-1.11.1.min.js"></script>
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/leaflet/leaflet.css" />
+	<script src="<?php echo base_url();?>assets/leaflet/leaflet.js"></script>
+	<script src="<?php echo base_url();?>assets/leaflet/leaflet-src.js"></script>
 	
 </head>
